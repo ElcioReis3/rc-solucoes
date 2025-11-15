1cf9f347448364001de61b90538bd9fc45fc1e39
Élcio Reis <elcio.reis024@gmail.com>
2025-11-15 07:36:28 -0300

    add to footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export const metadata: Metadata = {
     type: "website",
     images: [
       {
-        url: "https://rc-solucoes-produtos-limpeza.netlify.app/assets/images/logo-marca-dark.png",
+        url: "https://rc-solucoes-produtos-limpeza.netlify.app/assets/images/ogo-tipo-light.png",
         width: 800,
         height: 800,
         alt: "Logo RC Soluções",
--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -3,7 +3,7 @@ export const Footer = () => {
     <>
       <footer className="bg-gray-900 text-white py-6">
         <div className="container mx-auto px-4 text-center">
-          <a href="https://portifolio-elcio-reis.netlify.app/">
+          <a href="https://portifolio-elcio-reis.netlify.app/" target="_blank">
             <p>&copy; 2025 Elcio_Serviços_On - Todos os direitos reservados.</p>
           </a>
         </div>
